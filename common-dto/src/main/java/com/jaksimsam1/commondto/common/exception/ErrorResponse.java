5aca43808a90212a8b72e01c55513deb3009069e
ljm88 <ljm88@evsis.co.kr>
2024-10-05 22:39:31 +0900

    [feat] Common-Dto 공통 모듈 추가 - 공통 예외 - 메소드 로깅

---
+++ common-dto/src/main/java/com/jaksimsam1/commondto/common/exception/ErrorResponse.java
@@ -0,0 +1,37 @@
+package com.jaksimsam1.commondto.common.exception;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ErrorResponse {
+    private String message;
+    private int status;
+    private String code;
+    private List<FieldError> fieldErrors;
+
+    private ErrorResponse(ErrorCode errorCode, List<FieldError> fieldErrors) {
+        this.message = errorCode.getMessage();
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getCode();
+        this.fieldErrors = fieldErrors;
+    }
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.status = errorCode.getStatus();
+        this.message = errorCode.getMessage();
+        this.fieldErrors = new ArrayList<>();
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, BindingResult bindingResult) {
+        return new ErrorResponse(errorCode, bindingResult.getFieldErrors());
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode);
+    }
+
+}

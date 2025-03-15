6b438654e8c4b250d0bd35137fda0e5895276ecf
jmjang88 <jmjang88@naver.com>
2025-03-15 23:25:39 +0900

    [refactor] common-dto

---
+++ user-service/src/main/java/com/jaksimsam1/userservice/user/model/enums/Gender.java
@@ -0,0 +1,20 @@
+package com.jaksimsam1.userservice.user.model.enums;
+
+
+public enum Gender {
+    MALE("MALE"),
+    FEMALE("FEMALE");
+    ;
+
+    private final String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+
+    public static String fromValue(String value) {
+        if (value.equals(MALE.value)) return MALE.value;
+
+        return FEMALE.value;
+    }
+}

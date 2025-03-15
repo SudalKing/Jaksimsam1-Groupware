54194f9461b8a856bd70d1364eec8f24fb2182d3
jmjang88 <jmjang88@naver.com>
2025-03-15 23:31:33 +0900

    [fix] User-Service Redis 추가 - Redis 캐싱 커스텀 어노테이션 추가

--- user-service/src/main/java/com/jaksimsam1/userservice/config/redis/RedisCacheUtil.java
+++ user-service/src/main/java/com/jaksimsam1/userservice/config/redis/RedisCacheUtil.java
@@ -1,7 +1,6 @@
 package com.jaksimsam1.userservice.config.redis;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -9,41 +8,41 @@ import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
-@Slf4j
 @RequiredArgsConstructor
 @Component
 public class RedisCacheUtil {
     private final ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
 
-    public <T> Mono<T> cache(String key, Mono<T> value, Duration ttl) {
+    public <T> Mono<T> getCache(String key, Class<T> clazz) {
         return reactiveRedisTemplate.opsForValue()
                 .get(key)
-                .cast((Class<T>) value.getClass())
-                .switchIfEmpty(
-                        value.flatMap(result ->
-                            reactiveRedisTemplate.opsForValue()
-                                    .set(key, value, ttl)
-                                    .thenReturn(result)
-                        )
-                );
+                .cast(clazz);
     }
 
-    public <T> Flux<T> cache(String key, Flux<T> value, Duration ttl) {
+    public <T> Mono<T> setCache(String key, T value, Duration ttl) {
+        return reactiveRedisTemplate.opsForValue()
+                .set(key, value, ttl)
+                .thenReturn(value);
+    }
+
+    public Mono<Boolean> deleteCache(String key) {
+        return reactiveRedisTemplate.opsForValue()
+                .delete(key);
+    }
+
+    public <T> Flux<T> getCacheList(String key, Class<T> clazz) {
         return reactiveRedisTemplate.opsForList()
-                .size(key)
-                .flatMapMany(size -> {
-                    if (size > 0) {
-                        return reactiveRedisTemplate.opsForList()
-                                .range(key, 0, size - 1)
-                                .cast((Class<T>) value.getClass());
-                    } else {
-                        return value.collectList()
-                                .flatMapMany(list -> reactiveRedisTemplate.opsForList()
-                                        .rightPushAll(key, list)
-                                        .then(reactiveRedisTemplate.expire(key, ttl))
-                                        .thenMany(Flux.fromIterable(list))
-                                );
-                    }
-                });
+                .range(key, 0, -1)
+                .cast(clazz);
+    }
+
+    public <T> Flux<T> setCacheList(String key, Flux<T> values, Duration ttl) {
+        return values.collectList()
+                .flatMapMany(list ->
+                        reactiveRedisTemplate.opsForList()
+                                .rightPushAll(key, list)
+                                .then(reactiveRedisTemplate.expire(key, ttl))
+                                .thenMany(Flux.fromIterable(list))
+                );
     }
 }

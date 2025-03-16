5b4f77e70f936164cbdc990788de2e69ba2b5234
jmjang88 <jmjang88@naver.com>
2025-03-16 17:33:54 +0900

    [refactor] 리팩토링

--- auth-service/src/main/java/com/jaksimsam1/authservice/config/eureka/EurekaConfig.java
+++ auth-service/src/main/java/com/jaksimsam1/authservice/config/eureka/EurekaConfig.java
@@ -1,4 +1,4 @@
-package com.jaksimsam1.authservice.config;
+package com.jaksimsam1.authservice.config.eureka;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
--- auth-service/src/main/java/com/jaksimsam1/authservice/config/router/RouterConfig.java
+++ auth-service/src/main/java/com/jaksimsam1/authservice/config/router/RouterConfig.java
@@ -1,7 +1,8 @@
-package com.jaksimsam1.authservice.config;
+package com.jaksimsam1.authservice.config.router;
 
 import com.jaksimsam1.authservice.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -11,18 +12,17 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
+@Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class RouterConfig {
 
     private final AuthService authService;
-    private final RouterFilterConfig routerFilterConfig;
 
     @Bean
     public RouterFunction<ServerResponse> loginRoutes() {
         return RouterFunctions.route()
                 .POST("/auth/api/v1/login", accept(MediaType.APPLICATION_JSON), authService::login)
-//                .filter(routerFilterConfig::routerFilter)
                 .build();
     }
 }

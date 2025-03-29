2841d0f0b56ab154195734d914a30627a903f068
jmjang88 <jmjang88@naver.com>
2025-03-29 18:36:43 +0900

    [refactor] common-dto 제거

--- common-dto/src/main/java/com/jaksimsam1/commondto/common/kafka/KafkaConsumerGroup.java
+++
@@ -1,5 +0,0 @@
-package com.jaksimsam1.commondto.common.kafka;
-
-public class KafkaConsumerGroup {
-    public static final String USER_CREATED_GROUP = "user-created-group";
-}
--- common-dto/src/main/java/com/jaksimsam1/commondto/model/enums/ApiResponseCode.java
+++
@@ -1,7 +0,0 @@
-package com.jaksimsam1.commondto.model.enums;
-
-public interface ApiResponseCode {
-    int getStatus();
-    String getMessage();
-    String getDescription();
-}
